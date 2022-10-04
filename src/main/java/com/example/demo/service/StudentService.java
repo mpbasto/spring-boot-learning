c727d2079d774c5f2a60d68116b6f21660ff1fe0
Marta Basto <marta.basto@xdesign.com>
2022-10-04 22:18:28 +0100

    Create repository and PostgreSQL database

--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -1,22 +1,33 @@
 package com.example.demo.service;
 
 import com.example.demo.model.StudentModel;
+import com.example.demo.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+// import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<StudentModel> getStudents() {
-        return List.of(
-                new StudentModel(
-                        1L,
-                        "Mary",
-                        "mary.jammy@gmail.com",
-                        LocalDate.of(1989, 01,12),
-                        21
-                )
-        );
+        return studentRepository.findAll();
     }
 }
+
+
+// To be added
+//new StudentModel(
+//        1L,
+//        "Mary",
+//        "mary.jammy@gmail.com",
+//        LocalDate.of(1989, 01,12),
+//        21
+//        )
